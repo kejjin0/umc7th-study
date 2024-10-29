e90621f970323a4dbb29641d06d6096cf2eb19cc
kejjin0 <lala0830@naver.com>
2024-10-29 21:11:34 +0900

    answerAlarm관련 domain(@PromaryKeyJoinColumn -> @JoinColumn)

--- src/main/java/umc/spring/domain/AskAnswerAlarm.java
+++ src/main/java/umc/spring/domain/AskAnswerAlarm.java
@@ -30,6 +30,6 @@ public class AskAnswerAlarm extends BaseEntity {
     private ConfirmStatus confirm;
 
     @OneToOne
-    @PrimaryKeyJoinColumn(name="answer_id")
+    @JoinColumn(name="answer_id")
     private AskAnswer askAnswer;
 }

c82a639e2e7f802008782d3abe18baf1b91e022e
kejjin0 <lala0830@naver.com>
2024-11-20 00:02:13 +0900

    ✨ [FEAT] 가게추가 API 구현
    week8 미션 1번

---
+++ src/main/java/umc/spring/service/StoreService/StoreCommandServiceImpl.java
@@ -0,0 +1,30 @@
+package umc.spring.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.exception.handler.RegionHandler;
+import umc.spring.converter.StoreConverter;
+import umc.spring.domain.Region;
+import umc.spring.domain.Store;
+import umc.spring.dto.StoreDTO.StoreRequestDTO;
+import umc.spring.repository.RegionRepository.RegionRepository;
+import umc.spring.repository.StoreRepository.StoreRepository;
+
+@Service
+@RequiredArgsConstructor
+public class StoreCommandServiceImpl implements StoreCommandService {
+    private final StoreRepository storeRepository;
+    private final RegionRepository regionRepository;
+
+    @Override
+    @Transactional
+    public Store addStore(StoreRequestDTO.AddStoreDTO request){
+        Store newStore = StoreConverter.ToStore(request);
+        Region Region = regionRepository.findById(request.getRegion())
+                .orElseThrow(() -> new RegionHandler(ErrorStatus.REGION_NOT_FOUND));
+
+        return storeRepository.save(newStore);
+    }
+}

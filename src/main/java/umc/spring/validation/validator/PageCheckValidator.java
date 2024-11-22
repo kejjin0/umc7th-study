95d22f9bd0c4e4057cdb5a35ccc74db06cd9e093
kejjin0 <lala0830@naver.com>
2024-11-22 22:32:40 +0900

    ✨ [Feat] PageCheck 커스텀 어노체이션 추가 (page-1 기능 제외)
    추후 수정 예정

---
+++ src/main/java/umc/spring/validation/validator/PageCheckValidator.java
@@ -0,0 +1,27 @@
+package umc.spring.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.validation.annotation.CheckPage;
+
+@Component
+@RequiredArgsConstructor
+public class PageCheckValidator implements ConstraintValidator<CheckPage, Integer> {
+    @Override
+    public void initialize(CheckPage constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        boolean isValid = value > 0;
+        if(!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.OUT_OF_PAGE_INDEX.toString()).addConstraintViolation();
+        }
+        return isValid;
+    }
+}

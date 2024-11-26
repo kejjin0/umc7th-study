a628609b9a3e5d1e339958370cf0b17ff92a03e8
kejjin0 <lala0830@naver.com>
2024-11-26 23:56:31 +0900

    ✨ Feat: Spring Security를 활용한 로그인 및 회원가입 구현 #7
    week10 - 실습 1

--- src/main/generated/umc/spring/domain/QMember.java
+++ src/main/generated/umc/spring/domain/QMember.java
@@ -51,9 +51,13 @@ public class QMember extends EntityPathBase<Member> {
 
     public final StringPath nickname = createString("nickname");
 
+    public final StringPath password = createString("password");
+
     public final NumberPath<Integer> point = createNumber("point", Integer.class);
 
     public final ListPath<Review, QReview> reviewList = this.<Review, QReview>createList("reviewList", Review.class, QReview.class, PathInits.DIRECT2);
+
+    public final EnumPath<umc.spring.domain.enums.Role> role = createEnum("role", umc.spring.domain.enums.Role.class);
 
     public final EnumPath<umc.spring.domain.enums.SocialType> socialType = createEnum("socialType", umc.spring.domain.enums.SocialType.class);
 

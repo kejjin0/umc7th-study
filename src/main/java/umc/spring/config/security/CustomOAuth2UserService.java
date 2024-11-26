29fca8b2d6770d9c521b17c45d521dd198de2e01
kejjin0 <lala0830@naver.com>
2024-11-27 01:27:06 +0900

    ✨ Feat: OAuth2를 이용한 카카오톡 소셜 로그인 구현 #7
    week10 - 실습2

---
+++ src/main/java/umc/spring/config/security/CustomOAuth2UserService.java
@@ -0,0 +1,65 @@
+package umc.spring.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import umc.spring.domain.Member;
+import umc.spring.domain.enums.Gender;
+import umc.spring.domain.enums.Role;
+import umc.spring.repository.MemberRepository.MemberRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+
+        String nickname = (String) properties.get("nickname");
+        String email = nickname + "@kakao.com";
+
+        Member member = saveOrUpdateUser(email,nickname);
+
+        Map<String, Object> modifiedAttributes = new HashMap<>(attributes);
+        modifiedAttributes.put("email", email);
+
+        return new DefaultOAuth2User(
+                oAuth2User.getAuthorities(),
+                modifiedAttributes,
+                "email"
+        );
+    }
+
+    private Member saveOrUpdateUser(String email, String nickname) {
+        Member member = memberRepository.findByEmail(email)
+                .orElse(Member.builder()
+                        .email(email)
+                        .name(nickname)
+                        .password(passwordEncoder.encode("OAUTH_USER" + UUID.randomUUID()))
+                        .gender(Gender.NONE)
+                        .address("소셜로그인")
+                        .specAddress("소셜로그인")
+                        .birthyYear(2011)
+                        .birthyMonth(11)
+                        .birthyDay(11)
+                        .role(Role.USER)
+                        .build());
+        return memberRepository.save(member);
+    }
+}
